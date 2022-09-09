d4c3f794c52894af02648f9dd0756d8c7f44b713
Akeem <moshoodakeemolayinka@gmail.com>
2022-09-09 22:41:22 +0100

    delete file

--- src/__tests__/auth.test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest")
-const app = require("../../index")
-
-describe("register", () => {
-    it("return status code", async () => {
-        const res = await request(app).get("/");
-        expect(res.statusCode).toEqual(201)
-    })
-})

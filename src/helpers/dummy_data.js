835aec2dc01738167d468f7d66b591b00a6ff286
Akeem <moshoodakeemolayinka@gmail.com>
2022-09-09 22:39:15 +0100

    add dummy data

---
+++ src/helpers/dummy_data.js
@@ -0,0 +1,85 @@
+const dummy = {
+    "orgName" : "Lendqr",
+    "userName" : "John123",
+    "password" : "password",
+    "email" : "joh1n@gmail.com",
+    "phoneNumber" : 912344556,
+    "profile" : {
+        "firstName" : "John",
+        "lastName" : "Doe",
+        "avatar" : "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/725.jpg",
+        "gender" : "Male",
+        "bvn" : "123456789",
+        "address" : "Islan, lagos state.",
+        "currency" : "NGR"
+    },
+    "accountBalance" : 123,
+    "accountNumber" : "1234566788",
+    "guarantor" : {
+        "firstName" : "Lam",
+        "lastName" : "Gold",
+        "address" : "Islan, lagos state.",
+        "gender" : "Female",
+        "phoneNumber" : 12345667888
+    },
+    "socials" : {
+        "facebook" : "www.facebook.com/lendsqr",
+        "twitter" : "www.twitter.com/lendsqr",
+        "linkedIn" : "www.linkedIl.com/lendsqr"
+    },
+    "education" : {
+        "level" : "BSC",
+        "employmentStatus" : "Freelance",
+        "duration" : "2 years",
+        "officeEmail" : "lendsqr@gmail.com",
+         "monthlyIncome" : {
+            "0" : "1234",
+            "1" : "109887",
+            "loanRepayment" : "123456789"
+        }
+    }
+}
+
+const new_dummy_data = {
+    "orgName" : "Lendqr",
+    "userName" : "John1235",
+    "password" : "password",
+    "email" : "john123@gmail.com",
+    "phoneNumber" : 912344557,
+    "profile" : {
+        "firstName" : "John",
+        "lastName" : "Doe",
+        "avatar" : "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/725.jpg",
+        "gender" : "Male",
+        "bvn" : "122456789",
+        "address" : "Islan, lagos state.",
+        "currency" : "NGR"
+    },
+    "accountBalance" : 123,
+    "accountNumber" : "1234563788",
+    "guarantor" : {
+        "firstName" : "Lam",
+        "lastName" : "Gold",
+        "address" : "Islan, lagos state.",
+        "gender" : "Female",
+        "phoneNumber" : 12345667888
+    },
+    "socials" : {
+        "facebook" : "www.facebook.com/lendsqr",
+        "twitter" : "www.twitter.com/lendsqr",
+        "linkedIn" : "www.linkedIl.com/lendsqr"
+    },
+    "education" : {
+        "level" : "BSC",
+        "employmentStatus" : "Freelance",
+        "duration" : "2 years",
+        "officeEmail" : "lendsqr@gmail.com",
+         "monthlyIncome" : {
+            "0" : "1234",
+            "1" : "109887",
+            "loanRepayment" : "123456789"
+        }
+    }
+}
+
+module.exports = {dummy, new_dummy_data}
